e82f44f518d76930054849d4c648f84f821e3565
Rayane Reis <rayanereismota2@gmail.com>
2024-05-23 14:50:24 -0300

    Add produtos-destaque scripts for styling buttons on click

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const navMenuButton = document.querySelectorAll('.button-menu i');
 const searchBar = document.querySelector('.search-bar');
 const searchBarButton = document.querySelector('.button-search i');
 
+
 navMenuButton.forEach((button) => {
     button.onclick = function () {
         navMenu.classList.toggle('open');
@@ -16,3 +17,32 @@ searchBarButton.onclick = function () {
     searchBarButton.classList.toggle('fa-x');
     searchBar.classList.toggle('open');
 }
+
+/* Produtos em destaque */
+const linhaProdutosButtons = document.querySelectorAll('.linha-list__button');
+const lilyButton = document.querySelector('#lily');
+const makebButton = document.querySelector('#make-b');
+const miniButton = document.querySelector('#mini');
+
+
+lilyButton.addEventListener('click', () => {
+    alterarListaDeProdutos();
+    lilyButton.classList.add('active');
+})
+
+makebButton.addEventListener('click', () => {
+    alterarListaDeProdutos();
+    makebButton.classList.add('active');
+})
+
+miniButton.addEventListener('click', () => {
+    alterarListaDeProdutos();
+    miniButton.classList.add('active');
+})
+
+function alterarListaDeProdutos () {
+    linhaProdutosButtons.forEach((linhaButton) => {
+        linhaButton.classList.remove('active');
+    })
+}
+
